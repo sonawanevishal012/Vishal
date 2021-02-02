10bc9913a89fe5373526efb61bb88d0fce8a3dcc
Vishal Sonawane <55478141+sonawanevishal012@users.noreply.github.com>
2021-02-02 18:09:39 +0530

    Vishal Sonawane
    Designation

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Full-Stack Developer"],
+        strings: ["Web Developer", "Software Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Full-Stack Developer"],
+        strings: ["Web Developer", "Software Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
